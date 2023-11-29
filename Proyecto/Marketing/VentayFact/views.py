f2a6f171f9090cef04763648f00b739e4f0b725c
OrianaEsc <142623367+OrianaEsc@users.noreply.github.com>
2023-11-29 15:57:09 -0300

    intento-form

--- Proyecto/Marketing/VentayFact/views.py
+++ Proyecto/Marketing/VentayFact/views.py
@@ -1,8 +1,19 @@
+
+
 from itertools import product
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from VentayFact.models import Productos
 from .form import FormClientes
+
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth import login
+
+# #nose
+from django.contrib.auth.models import Group
+from imaplib import _Authenticator
+from django.contrib import messages # Para mensajes de sesión
+
 #EXEQUIEL
 from .carro import carro
 from django.shortcuts import redirect
@@ -39,15 +50,66 @@ def login(request):
     return render (request,'login.html', datos)
 
 def registrarse(request):
-    datos = {'form': FormClientes(request.POST)}
-    return render (request, 'registrarse.html', datos)
+    # datos = {'form': FormClientes(request.POST)}
+    # return render (request, 'registrarse.html', datos)
+    
 
-def carrito(request):
-    datos = {}
-    carrito = carro(request)
-    # carro = carrito.get_carrito()
-    print('carro', carrito.get_carrito())
-    return render (request, 'carrito.html', datos)
+#     def get (self, request):
+#         form = UserCreationForm()
+#         return render (request, 'registrarse.html', {"form" : form })
+#         # datos = {'form': FormClientes(request.POST)}
+#         # return render (request, 'registrarse.html', datos)
+# #     def post(self, request): 
+# #         form = UserCreationForm(request.post)
+
+# #         if form.is_valid():
+
+# #             usuario= form.save()
+
+# #             login(request, usuario)
+
+# #             return redirect('inicio')
+        
+# #         else: 
+# #             pass
+
+    if request.method == 'GET':
+        return render(request, 'registrarse.html', {'form': FormClientes})
+
+    if request.method == 'POST':
+        form = FormClientes(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            
+            group = Group.objects.get(name='Clientes')
+            user.groups.add(group)
+
+            # Autenticación manual del usuario creado
+            user = _Authenticator(
+                email = form.cleaned_data['email'],
+                password = form.cleaned_data['password1']       
+            )
+            login(request, user)
+
+            # Redireccion a la pagina principal
+            return redirect('inicio.html')
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages)
+
+                return render(request, "inicio.html", { "form", {form}})
+
+
+
+
+
+# def carrito(request):
+#     datos = {}
+#     carrito = carro(request)
+#     # carro = carrito.get_carrito()
+#     print('carro', carrito.get_carrito())
+#     return render (request, 'carrito.html', datos)
 
 # EXEQUIEL
 
